10cee29f38e88804dab888a17dd57a1ce0d73354
juliovalenciag <valenciagranadaj@gmail.com>
2024-04-14 07:58:42 -0600

    Por cambiar

--- src/app/(pages)/sign-up/page.tsx
+++ src/app/(pages)/sign-up/page.tsx
@@ -1,23 +1,69 @@
-"use client";
-
-import React, {useState} from 'react';
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
-import {Label} from "@/components/ui/label";
-import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
-import Link from "next/link";
-import {Apple, Eye, EyeOff, Home} from "lucide-react";
+"use client"
+import React, {useState, useEffect} from 'react';
+import {useRouter} from 'next/navigation';
+import Link from 'next/link';
+import {NextPage} from 'next';
+import {Apple, Eye, EyeOff, Home} from 'lucide-react';
+import {Card, CardHeader, CardContent, CardTitle, CardDescription, CardFooter} from '@/components/ui/card';
+import {Select, SelectTrigger, SelectContent, SelectItem, SelectValue} from '@/components/ui/select';
+import {Button} from '@/components/ui/button';
+import {Input} from '@/components/ui/input';
+import {Label} from '@/components/ui/label';
 import {Separator} from "@/components/ui/separator";
 
-const RegisterPage = () => {
-
+const RegisterPage: NextPage = () => {
+    const router = useRouter();
     const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        userType: ''
+    });
 
     const togglePasswordVisibility = () => setShowPassword(!showPassword);
 
-  return (
-    <div className="flex min-h-screen items-center justify-center  px-4">
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target; // Cambio de currentTarget a target
+    setFormData(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      alert('Passwords do not match.');
+      return;
+    }
+
+    try {
+      const response = await fetch('https://foodlinker-921f63db6b7b.herokuapp.com/create-user/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: formData.username,
+          email: formData.email,
+          password: formData.password,
+          // Asegúrate de enviar solo los campos requeridos por tu API
+        })
+      });
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log('Registration successful', data);
+        router.push('/auth/login'); // Actualiza esto a tu ruta de login
+      } else {
+        console.error('Registration failed', data);
+        alert(data.message || 'An unexpected error occurred');
+      }
+    } catch (error) {
+      console.error('Network error', error);
+      alert('Network error');
+    }
+  };
+
+    return (
+        <div className="flex min-h-screen items-center justify-center  px-4">
 
             <Card className="w-[350px]">
                 <CardHeader>
@@ -28,7 +74,7 @@ const RegisterPage = () => {
                             <p className="text-3xl font-bold">dLinker</p>
                         </aside>
                         <div className="absolute top-0 left-0 p-4">
-                            <Link href="/"><Home/></Link>
+                            <Link href="/public"><Home/></Link>
                         </div>
                     </Link>
                     <Separator/>
@@ -38,7 +84,7 @@ const RegisterPage = () => {
                     <CardDescription>Get Started</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <form>
+                    <form onSubmit={handleRegister}>
                         <div className="grid w-full items-center gap-4">
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="name">Name</Label>
@@ -61,7 +107,8 @@ const RegisterPage = () => {
 
                             <div className="flex flex-col space-y-1.5 relative">
                                 <Label htmlFor="password">Confirm Password</Label>
-                                <Input type={showPassword ? "text" : "password"} id="password" placeholder="Confirm Password"/>
+                                <Input type={showPassword ? "text" : "password"} id="password"
+                                       placeholder="Confirm Password"/>
                                 <span className="absolute inset-y-8 right-0 flex items-center pr-3">
                                     <button type="button" onClick={togglePasswordVisibility}
                                             className="text-gray-500 hover:text-gray-700">
@@ -96,14 +143,14 @@ const RegisterPage = () => {
                             <Link href='/sign-in'>Sign In</Link>
                         </Button>
                     </div>
-                    <Button className="group-hover:bg-amber-400">
+                    <Button className="group-hover:bg-amber-400" type="submit">
                         <Link href="/sign-in" className="text-white">Sign Up</Link>
                     </Button>
                 </CardFooter>
             </Card>
 
-    </div>
-  );
+        </div>
+    );
 };
 
 export default RegisterPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {ThemeProvider} from "@/providers/theme-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { SessionProvider } from "next-auth/react"; // Importar SessionProvider
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,14 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
+
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+
       </body>
     </html>
   );
